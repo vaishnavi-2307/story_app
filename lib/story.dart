2cc9c0a7ca0c6561ac9042ee9e9546850ce69b46
vaishnavi-2307 <vaishnaviambidi.23@gmail.com>
2021-11-05 15:07:40 +0530

    first commit

---
+++ lib/story.dart
@@ -0,0 +1,7 @@
+class Story {
+  String storyTitle;
+  String choice1;
+  String choice2;
+  Story({this.storyTitle, this.choice1, this.choice2});
+  //i have to add the {} for access to the private object
+}
